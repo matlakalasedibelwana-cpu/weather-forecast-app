1a0edb44dbacb798df18c632be67a765815962f1
Matlakala Sedibelwana <matlakalasedibelwana@gmail.com>
2026-06-11 13:45:17 +0200

    Improved the HTML and added weather and location function to the JavaScript

--- src/script.js
+++ src/script.js
@@ -1,10 +1,26 @@
+function showWeather(response) {
+  let temperatureElement = document.querySelector("#current-temperature");
+  let temperature = response.data.temperature.current;
+  let locationElement = document.querySelector("#city");
+
+  locationElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchLocation(city) {
+  let apiKey = "0c03ba179oef7df9dt732c8467e7b15c";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
-
   let searchInput = document.querySelector("#search-input");
-  let locationElement = document.querySelector("#weather-location");
-  locationElement.innerHTML = searchInput.value;
+
+  searchLocation(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchLocation("Johannesburg");
