ded2da91af020da88e1f24bd0b3dc0a36805d4b9
Matlakala Sedibelwana <matlakalasedibelwana@gmail.com>
2026-06-11 14:36:51 +0200

    Inserted more weather information

--- src/script.js
+++ src/script.js
@@ -2,9 +2,41 @@ function showWeather(response) {
   let temperatureElement = document.querySelector("#current-temperature");
   let temperature = response.data.temperature.current;
   let locationElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#current-condition");
+  let humidityElement = document.querySelector("#current-humidity");
+  let windSpeedElement = document.querySelector("#current-windspeed");
+  let feelsLikeElement = document.querySelector("#feels-like-temperature");
+  let feelsLikeTemperature = response.data.temperature.feels_like;
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
 
   locationElement.innerHTML = response.data.city;
+  timeElement.innerHTML = formatDate(date);
   temperatureElement.innerHTML = Math.round(temperature);
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+  feelsLikeElement.innerHTML = Math.round(feelsLikeTemperature);
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
 }
 
 function searchLocation(city) {
